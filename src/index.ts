0a8a9ede51abdf72852c080e4471b2e7ad8b0fba
Marto_0 <martvanenck1@gmail.com>
2020-10-24 17:39:34 +0200

    firebase init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
+import Discord from 'discord.js'
+import firebaseAdmin from 'firebase-admin'
+
+interface initOptions {
+	bot:Discord.Client
+	port:number
+	firebaseCredentials:firebaseAdmin.ServiceAccount
+}
+
+let db:any
+
+export function init(options:initOptions) {
+
+	
+	firebaseAdmin.initializeApp({
+		credential: firebaseAdmin.credential.cert(options.firebaseCredentials),
+	})
+
+	db = firebaseAdmin.firestore()
+
+	console.log('Database Initialized')
+}
+
 export function startDashboard(port:number, callback:Function) {
 	console.log('Yee')
 	callback()
